a08212c503d0a6422804b00dd07dc34362fb6ac6
Silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-24 08:09:45 +0430

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ function epoch() {
                 if (res.statusCode == 204) {
                     console.log("Message deleted\n");
                 } else {
-                    console.log(res.body);
                     console.log("Failed to delete message\n")
                 }
             })
